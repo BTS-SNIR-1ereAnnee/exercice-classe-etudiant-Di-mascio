50fb2cfe8097bdfd41f72532228403747bd9a6a3
Di-mascio <di-mascio.esteban@lyceeppr.fr>
2020-03-26 12:35:49 +0100

    creation des methode et des attribut

---
+++ src/classes/etudiant.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "etudiant.h"
+
+etudiant::etudiant()
+{
+
+}
+
+void etudiant::getEtudiant()
+{
+
+}
+
+void etudiant::setEtudiant()
+{
+
+}
+
+float etudiant::getNote()
+{
+
+}
+
+float etudiant::setNote()
+{
+
+}
+
+float etudiant::getMoyenne()
+{
+
+}
+
+float etudiant::setMoyenne()
+{
+
+}
--- src/classes/etudiant.h
+++ src/classes/etudiant.h
@@ -3,6 +3,17 @@
 class etudiant
 {
 	public:
-	
+		etudiant();
+
+		void getEtudiant();
+		void setEtudiant();
+		float getNote();
+		float setNote();
+		float getMoyenne();
+		float setMoyenne();
+
 	private:
+		char m_etudiant[20];
+		float m_note;
+		float m_moyenne;
 };
